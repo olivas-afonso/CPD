961a09fccf51b7c8b61d719dbf173ccb69056e5b
olivas-afonso <olivas.afonso@gmail.com>
2024-02-18 18:41:07 +0000

    v1

--- world_gen_olivas.c
+++ world_gen_olivas.c
@@ -5,6 +5,13 @@
 
 char *** grid;
 unsigned int seed;
+long long cont_species[9]={0,0,0,0,0,0,0,0,0};
+//long long cont1=0, cont2=0, cont3=0, cont4=0, cont5=0, cont6=0, cont7=0, cont8=0, cont9=0;
+//long long max_cont1, max_cont2, max_cont3, max_cont4, max_cont5, max_cont6, max_cont7, max_cont8, max_cont9;
+long long max_count[9]={0,0,0,0,0,0,0,0,0};
+int max_gen[9];
+int current_gen =0;
+//int max_gen1, max_gen2, max_gen3, max_gen4, max_gen5, max_gen6, max_gen7, max_gen8, max_gen9; 
 
 void init_r4uni(int input_seed)
 {
@@ -56,15 +63,96 @@ char ***gen_initial_grid(long long N, float density, int input_seed)
     return grid;
 }
 
+void search_species_line(long long N, char * line) // search linha
+{
+    long long aux;
+
+    for(aux =0; aux<N; aux++)
+    {
+        switch (line[aux])
+        {
+        case 1:
+            cont_species[0]++;
+            break;
+        
+        case 2:
+            cont_species[1]++;
+            break;
+
+        case 3:
+            cont_species[2]++;
+            break;
+
+        case 4:
+            cont_species[3]++;
+            break;
+
+        case 5:
+            cont_species[4]++;
+            break;
+
+        case 6:
+            cont_species[5]++;
+            break;
+
+        case 7:
+            cont_species[6]++;
+            break;
+
+        case 8:
+            cont_species[7]++;
+            break;
+        
+        case 9:
+            cont_species[8]++;
+            break;
+        
+        default:
+            break;
+        }
+    }
+    
+}
+
+void life_rules(long long N, char *** grid)
+{
+    long long aux;
+    for(aux=0; aux< N; aux ++)
+    {
+        
+    }
+}
+
 int main(int argc, char *argv[]) {
    // printf() displays the string inside quotation
-
+    long long contx, conty; 
+    int auxi;
    // primeiro numero gen, segundo nº celulas por lado do cubo, terceiro densidade initial, quarto seed
    printf("Hello, World!\n");
    //printf("%s\n", argv[1]);
 
    grid = gen_initial_grid(4, .4, 100);
-   printf("Value :%d\n", grid[0][0][3]);
+   printf("Value :%d\n", grid[0][0][0]);
+
+    for(contx=0; contx < 4; contx++){
+        for(conty=0; conty < 4; conty++){
+            search_species_line(4, grid[contx][conty]);
+        }
+    }
+
+    for(auxi=0; auxi < 9; auxi++)
+    {
+        if(cont_species[auxi] > max_count[auxi])
+        {
+           max_count[auxi] = cont_species[auxi];
+           max_gen[auxi]=current_gen;
+        }
+
+        printf("%d %llu %d\n", auxi+1, max_count[auxi], max_gen[auxi]);
+    }
+
+    
+    //printf("Count :%llu\n", cont_species[2]);
 
    return 0;
 }
