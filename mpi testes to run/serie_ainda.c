398962c4782b7afc1452a99260e6a4ebd467e849
Duarte Diamantino <62574192+Duarte-Diamantino@users.noreply.github.com>
2024-03-19 16:08:52 +0000

    ver grid antes

--- mpi testes to run/serie_ainda.c
+++ mpi testes to run/serie_ainda.c
@@ -351,6 +351,16 @@ int main(int argc, char *argv[]) {
         }
     
 	}
+	
+	printf("GRID\n")
+	for(int merda_z= 0; merda_x < NX; merda_z++){
+		for(int merda_y = 0; merda_y < NX; merda_y++){
+			for(int merda_x = 0; merda_x < NX; merda_x++){
+				printf("%d\n", grid[aux_z][aux_y][aux_x])
+			}			
+		}
+	}
+	
    // exec_time += omp_get_wtime();
     //fprintf(stderr, "%.1fs\n", exec_time);
     
