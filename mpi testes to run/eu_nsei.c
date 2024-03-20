3769831b835a42a7c930f0f8df01cbc00ca24e5f
olivas-afonso <olivas.afonso@gmail.com>
2024-03-20 18:15:32 +0000

    Update eu_nsei.c

--- mpi testes to run/eu_nsei.c
+++ mpi testes to run/eu_nsei.c
@@ -38,35 +38,30 @@ int main(int argc, char *argv[]) {
     MPI_Cart_shift(cart_comm, 2, 1, &forward_rank, &backward_rank);
 
     // Send and receive data between neighbors
-    int ***data_recv_up = (int ***)malloc(dims[0] * sizeof(int **));
-    int ***data_recv_down = (int ***)malloc(dims[0] * sizeof(int **));
-    int ***data_recv_left = (int ***)malloc(dims[0] * sizeof(int **));
-    int ***data_recv_right = (int ***)malloc(dims[0] * sizeof(int **));
-    int ***data_recv_forward = (int ***)malloc(dims[0] * sizeof(int **));
-    int ***data_recv_backward = (int ***)malloc(dims[0] * sizeof(int **));
-
-    MPI_Request reqs[6];
-    MPI_Status stats[6];
+    MPI_Request reqs[12];
+    MPI_Status stats[12];
 
     MPI_Isend(&(data_send[0][0][0]), dims[0]*dims[1]*dims[2], MPI_INT, up_rank, 0, cart_comm, &reqs[0]);
-    MPI_Irecv(&(data_recv_down[0][0][0]), dims[0]*dims[1]*dims[2], MPI_INT, down_rank, 0, cart_comm, &reqs[1]);
-    MPI_Isend(&(data_send[0][0][0]), dims[0]*dims[1]*dims[2], MPI_INT, down_rank, 0, cart_comm, &reqs[2]);
-    MPI_Irecv(&(data_recv_up[0][0][0]), dims[0]*dims[1]*dims[2], MPI_INT, up_rank, 0, cart_comm, &reqs[3]);
+    MPI_Irecv(&(data_send[dims[0] - 1][0][0]), dims[1]*dims[2], MPI_INT, down_rank, 0, cart_comm, &reqs[1]);
+    MPI_Isend(&(data_send[dims[0] - 1][0][0]), dims[1]*dims[2], MPI_INT, down_rank, 0, cart_comm, &reqs[2]);
+    MPI_Irecv(&(data_send[0][0][0]), dims[1]*dims[2], MPI_INT, up_rank, 0, cart_comm, &reqs[3]);
+
     MPI_Isend(&(data_send[0][0][0]), dims[0]*dims[1]*dims[2], MPI_INT, left_rank, 0, cart_comm, &reqs[4]);
-    MPI_Irecv(&(data_recv_right[0][0][0]), dims[0]*dims[1]*dims[2], MPI_INT, right_rank, 0, cart_comm, &reqs[5]);
-    MPI_Isend(&(data_send[0][0][0]), dims[0]*dims[1]*dims[2], MPI_INT, right_rank, 0, cart_comm, &reqs[6]);
-    MPI_Irecv(&(data_recv_left[0][0][0]), dims[0]*dims[1]*dims[2], MPI_INT, left_rank, 0, cart_comm, &reqs[7]);
+    MPI_Irecv(&(data_send[0][dims[1] - 1][0]), dims[0]*dims[2], MPI_INT, right_rank, 0, cart_comm, &reqs[5]);
+    MPI_Isend(&(data_send[0][dims[1] - 1][0]), dims[0]*dims[2], MPI_INT, right_rank, 0, cart_comm, &reqs[6]);
+    MPI_Irecv(&(data_send[0][0][0]), dims[0]*dims[2], MPI_INT, left_rank, 0, cart_comm, &reqs[7]);
+
     MPI_Isend(&(data_send[0][0][0]), dims[0]*dims[1]*dims[2], MPI_INT, forward_rank, 0, cart_comm, &reqs[8]);
-    MPI_Irecv(&(data_recv_backward[0][0][0]), dims[0]*dims[1]*dims[2], MPI_INT, backward_rank, 0, cart_comm, &reqs[9]);
-    MPI_Isend(&(data_send[0][0][0]), dims[0]*dims[1]*dims[2], MPI_INT, backward_rank, 0, cart_comm, &reqs[10]);
-    MPI_Irecv(&(data_recv_forward[0][0][0]), dims[0]*dims[1]*dims[2], MPI_INT, forward_rank, 0, cart_comm, &reqs[11]);
+    MPI_Irecv(&(data_send[0][0][dims[2] - 1]), dims[0]*dims[1], MPI_INT, backward_rank, 0, cart_comm, &reqs[9]);
+    MPI_Isend(&(data_send[0][0][dims[2] - 1]), dims[0]*dims[1], MPI_INT, backward_rank, 0, cart_comm, &reqs[10]);
+    MPI_Irecv(&(data_send[0][0][0]), dims[0]*dims[1], MPI_INT, forward_rank, 0, cart_comm, &reqs[11]);
 
     MPI_Waitall(12, reqs, stats);
 
     // Print received data
     printf("Process %d: Received from up: %d, down: %d, left: %d, right: %d, forward: %d, backward: %d\n", rank,
-           data_recv_up[0][0][0], data_recv_down[0][0][0], data_recv_left[0][0][0],
-           data_recv_right[0][0][0], data_recv_forward[0][0][0], data_recv_backward[0][0][0]);
+           data_send[0][0][0], data_send[dims[0] - 1][0][0], data_send[0][dims[1] - 1][0],
+           data_send[0][0][dims[2] - 1], data_send[0][0][0], data_send[0][0][dims[2] - 1]);
 
     // Free allocated memory
     for (int i = 0; i < dims[0]; ++i) {
@@ -76,29 +71,6 @@ int main(int argc, char *argv[]) {
         free(data_send[i]);
     }
     free(data_send);
-
-    for (int i = 0; i < dims[0]; ++i) {
-        for (int j = 0; j < dims[1]; ++j) {
-            free(data_recv_up[i][j]);
-            free(data_recv_down[i][j]);
-            free(data_recv_left[i][j]);
-            free(data_recv_right[i][j]);
-            free(data_recv_forward[i][j]);
-            free(data_recv_backward[i][j]);
-        }
-        free(data_recv_up[i]);
-        free(data_recv_down[i]);
-        free(data_recv_left[i]);
-        free(data_recv_right[i]);
-        free(data_recv_forward[i]);
-        free(data_recv_backward[i]);
-    }
-    free(data_recv_up);
-    free(data_recv_down);
-    free(data_recv_left);
-    free(data_recv_right);
-    free(data_recv_forward);
-    free(data_recv_backward);
 
     MPI_Finalize();
     return 0;
