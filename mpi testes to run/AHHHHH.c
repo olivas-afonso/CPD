887b7dcf82f2c714016394ef644fe46648dc4ba3
olivas-afonso <olivas.afonso@gmail.com>
2024-03-20 19:07:58 +0000

    Update AHHHHH.c

--- mpi testes to run/AHHHHH.c
+++ mpi testes to run/AHHHHH.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[]) {
         for (int j = 0; j < dims[1]; ++j) {
             data_send[i][j] = (int *)malloc(dims[2] * sizeof(int));
             for (int k = 0; k < dims[2]; ++k) {
-                data_send[i][j][k] = rank * 100 + i * 1000 + j * 10000 + k * 100000;  // Example data
+                data_send[i][j][k] = rank;  // Example data
             }
         }
     }
