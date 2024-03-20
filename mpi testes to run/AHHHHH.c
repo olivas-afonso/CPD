4a9d3e5c9c1492721bde9d043bb269e61c3f1114
olivas-afonso <olivas.afonso@gmail.com>
2024-03-20 22:15:23 +0000

    Update AHHHHH.c

--- mpi testes to run/AHHHHH.c
+++ mpi testes to run/AHHHHH.c
@@ -36,13 +36,13 @@ int main(int argc, char *argv[]) {
     
     //int my_coords_aux[3];
     MPI_Cart_coords(cart_comm, rank, 3, my_coords);
-
+    // IMPORTANTE: COORD 0 - CIMA, COORD 1
     // Get neighbors
     int up_rank, down_rank, left_rank, right_rank, forward_rank, backward_rank, diag_rank, source_rank;
     MPI_Cart_shift(cart_comm, 0, 1, &up_rank, &down_rank);  
     MPI_Cart_shift(cart_comm, 1, 1, &left_rank, &right_rank);
     MPI_Cart_shift(cart_comm, 2, 1, &forward_rank, &backward_rank);
-    MPI_Cart_shift(cart_comm, 0, 2, &source_rank, &diag_rank);
+    MPI_Cart_shift(cart_comm, 1, 2, &source_rank, &diag_rank);
     //My_MPI_Cart_Shift(cart_comm, 1, 1, 2, 1, &source_rank, &diag_rank);
 
     //int diagonal_coords[2] = {(my_coords[0] + 1) % dims[0], (my_coords[1] + 1) % dims[1]};
