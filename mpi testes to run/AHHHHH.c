b067413b522a86066cd3ddaefbb54ceb649ded41
olivas-afonso <olivas.afonso@gmail.com>
2024-03-20 18:50:38 +0000

    Update AHHHHH.c

--- mpi testes to run/AHHHHH.c
+++ mpi testes to run/AHHHHH.c
@@ -47,10 +47,11 @@ int main(int argc, char *argv[]) {
         }
     }
 
-
+    
     int data_recv_up, data_recv_left, data_recv_right, data_recv_forward, data_recv_backward;
     for(int aux=0; aux < dims[2]; aux++)
     {
+        printf("SUPPOSED TO SEND %d\n", data_send[0][aux]);
         MPI_Sendrecv(data_send[0][aux], dims[2], MPI_INT, up_rank, 0, data_recv_down[0][aux], dims[2], MPI_INT, down_rank, 0, cart_comm, MPI_STATUS_IGNORE);
     }
     
@@ -61,7 +62,7 @@ int main(int argc, char *argv[]) {
     //MPI_Sendrecv(&data_send, 1, MPI_INT, backward_rank, 0, &data_recv_forward, 1, MPI_INT, forward_rank, 0, cart_comm, MPI_STATUS_IGNORE);
 
     // Print received data
-    printf("Process %d, down: %d\n", rank, data_recv_down);
+    printf("Process %d, down: %d\n", rank, data_recv_down[0][aux]);
 
     MPI_Finalize();
     return 0;
