117a71d9d60160566313fff1f9b15ced1ac0c3b4
olivas-afonso <olivas.afonso@gmail.com>
2024-03-23 15:59:47 +0000

    Update AHHHHH.c

--- mpi testes to run/AHHHHH.c
+++ mpi testes to run/AHHHHH.c
@@ -132,7 +132,7 @@ int main(int argc, char *argv[]) {
         for (aux_y=0; aux_y<TAMANHO_GRID; aux_y++)
         {
             MPI_Sendrecv(&data_send[aux_z][aux_y][0], dims[2], MPI_INT, esq_rank, 0, &data_recv_dir[aux_z+1][aux_y], dims[2], MPI_INT, dir_rank, 0, cart_comm, MPI_STATUS_IGNORE); // face dir
-            //MPI_Sendrecv(&data_send[0][aux_y][0], dims[2], MPI_INT, esq_baixo_rank, 0, &data_recv_dir[0][aux_y][0], dims[2], MPI_INT, dir_cima_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR dir cima
+            MPI_Sendrecv(&data_send[0][aux_y][0], dims[2], MPI_INT, esq_baixo_rank, 0, &data_recv_dir[TAMANHO_GRID+1][aux_y], dims[2], MPI_INT, dir_cima_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR dir cima
             for(aux_x=0;aux_x<TAMANHO_GRID; aux_x++)
             {
                 //
@@ -172,11 +172,11 @@ int main(int argc, char *argv[]) {
 
     if(rank==13)
     {
-        for(aux_z=0;aux_z<TAMANHO_GRID;aux_z++)
+        for(aux_z=0;aux_z<(TAMANHO_GRID+2);aux_z++)
         {
             for(aux_y=0;aux_y<TAMANHO_GRID;aux_y++)
             {
-                printf("rank: %d, SUPPOSED TO RECEIVE FACE DIR (aux %d / %d) %d\n",rank,aux_z, aux_y, data_recv_dir[aux_z+1][aux_y]);
+                printf("rank: %d, SUPPOSED TO RECEIVE FACE DIR (aux %d / %d) %d\n",rank,aux_z, aux_y, data_recv_dir[aux_z][aux_y]);
             }
 
             //printf("rank: %d, SUPPOSED TO RECEIVE FRENTE CIMA (aux %d) %d\n",rank,aux, data_recv_down[0][0][aux]);
