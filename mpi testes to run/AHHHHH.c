d4910beb7e6af80d47bb28f27ec82774d6434d4a
olivas-afonso <olivas.afonso@gmail.com>
2024-03-23 15:02:36 +0000

    Update AHHHHH.c

--- mpi testes to run/AHHHHH.c
+++ mpi testes to run/AHHHHH.c
@@ -62,7 +62,8 @@ int main(int argc, char *argv[]) {
     //IMPORTANTE: ARESTA BAIXA ESQUERDA: X=N, Y=0, Z=N
     //IMPORTANTE: ARESTA BAIXA FRENTE: X MUDA, Y=N, Z=N
     // Get neighbors
-    int up_rank, down_rank, left_rank, right_rank, forward_rank, backward_rank, diag_rank, source_rank;
+    int up_rank, down_rank, left_rank, right_rank, forward_rank, backward_rank;
+    int dir_cima_rank,dir_frente_rank, source_rank;
     MPI_Cart_shift(cart_comm, 0, 1, &up_rank, &down_rank);  
     MPI_Cart_shift(cart_comm, 1, 1, &left_rank, &right_rank);
     MPI_Cart_shift(cart_comm, 2, 1, &forward_rank, &backward_rank);
@@ -110,12 +111,14 @@ int main(int argc, char *argv[]) {
     int aux;
     int data_recv_up, data_recv_left, data_recv_right, data_recv_forward, data_recv_backward;
     //My_MPI_Cart_Shift(cart_comm, 2, 1, 0, 0, 1, 1, &source_rank, &diag_rank); // frente cima
+    My_MPI_Cart_Shift(cart_comm, 2, 1, 0, 1, 0, 1, &source_rank, &dir_cima_rank); // dir tras cima
+    My_MPI_Cart_Shift(cart_comm, 2, 1, 0, 0, dims[2]-1, 1, &source_rank, &dir_frente_rank); // dir tras cima
     for( aux=0; aux < dims[2]; aux++)
     {
         //printf("rank: %d, SUPPOSED TO SEND %d\n",rank, data_send[0][aux]);
-        My_MPI_Cart_Shift(cart_comm, 2, 1, 0, 0, dims[2]-1, 1, &source_rank, &diag_rank); // dir tras cima
+        
         //My_MPI_Cart_Shift(cart_comm, 2, 1, 0, 1, 0, 1, &source_rank, &diag_rank); // dir cima
-        MPI_Sendrecv(&data_send[0][0][aux], dims[2], MPI_INT, source_rank, 0, &data_recv_down[0][0][aux], dims[2], MPI_INT, diag_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR cima dir
+        MPI_Sendrecv(&data_send[0][0][aux], dims[2], MPI_INT, source_rank, 0, &data_recv_down[0][0][aux], dims[2], MPI_INT, dir_cima_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR cima dir
         
         //MPI_Sendrecv(&data_send[0][2][aux], dims[2], MPI_INT, source_rank, 0, &data_recv_down[0][2][aux], dims[2], MPI_INT, diag_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR cima fre
         /*
