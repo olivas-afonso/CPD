287039338c92cb63638709aa5ad4049d9078909e
olivas-afonso <olivas.afonso@gmail.com>
2024-03-23 16:08:24 +0000

    Update AHHHHH.c

--- mpi testes to run/AHHHHH.c
+++ mpi testes to run/AHHHHH.c
@@ -134,7 +134,7 @@ int main(int argc, char *argv[]) {
         {
             MPI_Sendrecv(&data_send[aux_z][aux_y][0], dims[2], MPI_INT, esq_rank, 0, &data_recv_dir[aux_z+1][aux_y], dims[2], MPI_INT, dir_rank, 0, cart_comm, MPI_STATUS_IGNORE); // face dir
             MPI_Sendrecv(&data_send[0][aux_y][0], dims[2], MPI_INT, esq_baixo_rank, 0, &data_recv_dir[TAMANHO_GRID+1][aux_y], dims[2], MPI_INT, dir_cima_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR dir cima
-            MPI_Sendrecv(&data_send[TAMANHO_GRID-1][aux_y][TAMANHO_GRID-1], dims[2], MPI_INT, esq_cima_rank, 0, &data_recv_dir[0][aux_y], dims[2], MPI_INT, dir_baixo_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR dir cima
+            MPI_Sendrecv(&data_send[TAMANHO_GRID-1][aux_y][0], dims[2], MPI_INT, esq_cima_rank, 0, &data_recv_dir[0][aux_y], dims[2], MPI_INT, dir_baixo_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR dir cima
             for(aux_x=0;aux_x<TAMANHO_GRID; aux_x++)
             {
                 //
