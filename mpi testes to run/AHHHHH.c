e50c250414f6269d6007b53c12b7e1f3e186d1e3
olivas-afonso <olivas.afonso@gmail.com>
2024-03-23 14:02:31 +0000

    Update AHHHHH.c

--- mpi testes to run/AHHHHH.c
+++ mpi testes to run/AHHHHH.c
@@ -29,6 +29,7 @@ int main(int argc, char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
+    int count=0;
     // Define Cartesian Topology
     int dims[3] = {3, 3, 3};  // 2x2x2 grid
     int periods[3] = {1, 1, 1};  // Enable wraparound
@@ -85,7 +86,8 @@ int main(int argc, char *argv[]) {
         for (int j = 0; j < dims[1]; ++j) {
             data_send[i][j] = (int *)malloc(dims[2] * sizeof(int));
             for (int k = 0; k < dims[2]; ++k) {
-                data_send[i][j][k]=rank*1000 +k;
+                data_send[i][j][k]=rank*1000 +count;
+                count++;
                 //data_send[i][j][k] = rank + my_coords[0] * 100 + my_coords[1] * 1000 + my_coords[2] * 10000;  // Example data
             }
         }
