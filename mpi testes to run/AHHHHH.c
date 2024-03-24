12572e6a4d73202002ce97a309a6a395e108c58c
olivas-afonso <olivas.afonso@gmail.com>
2024-03-24 22:01:31 +0000

    Update AHHHHH.c

--- mpi testes to run/AHHHHH.c
+++ mpi testes to run/AHHHHH.c
@@ -166,7 +166,7 @@ int main(int argc, char *argv[]) {
     //VERT ESQ CIMA FRENTE
     
     MPI_Sendrecv(&data_send[0][0][TAMANHO_GRID-1], dims[2], MPI_INT, dir_baixo_tras_rank, 0, &vert_esq_cima_frente, dims[2], MPI_INT, esq_cima_frente_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR dir cima
-    //MPI_Sendrecv(&data_send[TAMANHO_GRID-1][TAMANHO_GRID-1][0], dims[2], MPI_INT, esq_cima_frente_rank, 0, &vert_dir_baixo_tras, dims[2], MPI_INT, dir_baixo_tras_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR dir cima
+    MPI_Sendrecv(&data_send[0][0][TAMANHO_GRID-1], dims[2], MPI_INT, dir_baixo_tras_rank, 0, &vert_dir_baixo_tras, dims[2], MPI_INT, esq_cima_frente_rank, 0, cart_comm, MPI_STATUS_IGNORE);
 
     for( aux_z=0; aux_z < TAMANHO_GRID; aux_z++)
     {
