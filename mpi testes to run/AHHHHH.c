d20c5e193c8ec4a91c01722f20434f91e188a4ec
olivas-afonso <olivas.afonso@gmail.com>
2024-03-24 21:56:15 +0000

    Update AHHHHH.c

--- mpi testes to run/AHHHHH.c
+++ mpi testes to run/AHHHHH.c
@@ -164,8 +164,10 @@ int main(int argc, char *argv[]) {
 
 
     //VERT ESQ CIMA FRENTE
-     MPI_Sendrecv(&data_send[TAMANHO_GRID-1][0][TAMANHO_GRID-1], dims[2], MPI_INT, dir_baixo_tras_rank, 0, &vert_esq_cima_frente, dims[2], MPI_INT, esq_cima_frente_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR dir cima
     
+    MPI_Sendrecv(&data_send[0][0][TAMANHO_GRID-1], dims[2], MPI_INT, dir_baixo_tras_rank, 0, &vert_esq_cima_frente, dims[2], MPI_INT, esq_cima_frente_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR dir cima
+    MPI_Sendrecv(&data_send[TAMANHO_GRID-1][TAMANHO_GRID-1][0], dims[2], MPI_INT, esq_cima_frente_rank, 0, &vert_dir_baixo_tras, dims[2], MPI_INT, dir_baixo_tras_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR dir cima
+
     for( aux_z=0; aux_z < TAMANHO_GRID; aux_z++)
     {
 
@@ -321,7 +323,10 @@ int main(int argc, char *argv[]) {
             }
         }
         printf("\n");
-         printf("rank: %d, SUPPOSED TO RECEIVE VERT ESQ CIMA FRENTE  %d\n",rank, vert_esq_cima_frente);
+        printf("rank: %d, SUPPOSED TO RECEIVE VERT ESQ CIMA FRENTE  %d\n",rank, vert_esq_cima_frente);
+
+        printf("\n");
+        printf("rank: %d, SUPPOSED TO RECEIVE VERT DIR BAIXO TRAS  %d\n",rank, vert_dir_baixo_tras);
     }
 
    
