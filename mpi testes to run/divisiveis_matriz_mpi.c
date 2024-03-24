5c670e1d2d88901707713255bc1f6db03415e70b
Duarte Diamantino <62574192+Duarte-Diamantino@users.noreply.github.com>
2024-03-24 18:51:00 +0000

    Update divisiveis_matriz_mpi.c

--- mpi testes to run/divisiveis_matriz_mpi.c
+++ mpi testes to run/divisiveis_matriz_mpi.c
@@ -165,13 +165,10 @@ char ***gen_initial_bloco(int max, int min ,int N, float density, int input_seed
 					prov = (int)(r4_uni() * N_SPECIES) + 1; // preenchimento initial do grid_even dependendo da seed
 					count_species[grid_even[x][y][z]]++;
 						
+					grid_even[x][y][z] = prov;
 					
-					if (aux < 2) {
-						grid_even[x][y][z] = prov;	
-					}
-					if(aux > 3 && aux < 6 ){
-						grid_even[x][y][z] = prov;	
-					}
+						
+					
 				}
 				
         }     
