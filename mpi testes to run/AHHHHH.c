4f6df98d64082d7ebffce814a8f3eed6405ecaa2
olivas-afonso <olivas.afonso@gmail.com>
2024-03-24 21:24:12 +0000

    Update AHHHHH.c

--- mpi testes to run/AHHHHH.c
+++ mpi testes to run/AHHHHH.c
@@ -160,8 +160,8 @@ int main(int argc, char *argv[]) {
         MPI_Sendrecv(&data_send[aux_z][TAMANHO_GRID-1][0], dims[2], MPI_INT, esq_frente_rank, 0, &data_recv_dir[TAMANHO_GRID+1][aux_z], dims[2], MPI_INT, dir_tras_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR dir baixo
 
         //FACE ESQUERDA DIAGS
-        MPI_Sendrecv(&data_send[TAMANHO_GRID-1][aux_z][TAMANHO_GRID-1], dims[2], MPI_INT, dir_cima_rank, 0, &data_recv_esq[0][aux_z], dims[2], MPI_INT, esq_baixo_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR esq baixo
-        MPI_Sendrecv(&data_send[0][aux_z][TAMANHO_GRID-1], dims[2], MPI_INT, dir_baixo_rank, 0, &data_recv_esq[TAMANHO_GRID+1][aux_z], dims[2], MPI_INT, esq_cima_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR esq baixo
+        MPI_Sendrecv(&data_send[aux_z][TAMANHO_GRID-1][0], dims[2], MPI_INT, dir_tras_rank, 0, &data_recv_esq[0][aux_z], dims[2], MPI_INT, esq_frente_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR esq baixo
+        MPI_Sendrecv(&data_send[aux_z][TAMANHO_GRID-1][0], dims[2], MPI_INT, dir_frente_rank, 0, &data_recv_esq[TAMANHO_GRID+1][aux_z], dims[2], MPI_INT, esq_tras_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR esq baixo
 
         //FACE CIMA DIAGS
         MPI_Sendrecv(&data_send[0][aux_z][TAMANHO_GRID-1], dims[2], MPI_INT, dir_baixo_rank, 0, &data_recv_cima[0][aux_z], dims[2], MPI_INT, esq_cima_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR esq baixo
