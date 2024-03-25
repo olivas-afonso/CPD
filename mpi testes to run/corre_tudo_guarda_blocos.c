5cd06daac25d37337def1ee4183c89638b1a77d9
Duarte Diamantino <62574192+Duarte-Diamantino@users.noreply.github.com>
2024-03-25 11:26:27 +0000

    Update corre_tudo_guarda_blocos.c

--- mpi testes to run/corre_tudo_guarda_blocos.c
+++ mpi testes to run/corre_tudo_guarda_blocos.c
@@ -223,10 +223,12 @@ char ***gen_initial_bloco(int max, int min ,int n,int N, float density, int inpu
 							prov = (int)(r4_uni() * N_SPECIES) + 1; // preenchimento initial do grid_even dependendo da seed
 							//count_species[grid_even[x][y][z]]++;
 							//printf("numero: %d\n",prov);	 	
+				}else{
+					prov = 0;
 				}
 				printf("celula lida: %d\n", prov);	
 				if (x>= min && y>= min && z>= min) {
-					if (x<max && y<max && z<max) {
+					if (x<max && y<max && z<max){
 							printf("entrou\n");
 							grid_even[x1][y1][z1] = prov;
 						z1 ++;
