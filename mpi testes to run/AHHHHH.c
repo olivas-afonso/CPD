2c0815c88337245b049c99d0121261c79630ad86
olivas-afonso <olivas.afonso@gmail.com>
2024-03-25 16:18:19 +0000

    Update AHHHHH.c

--- mpi testes to run/AHHHHH.c
+++ mpi testes to run/AHHHHH.c
@@ -179,15 +179,15 @@ int main(int argc, char *argv[]) {
 
     //VERT ESQ CIMA FRENTE
     if (rank==13) printf("START\n");
-    MPI_Sendrecv(&data_send[1][1][TAMANHO_GRID-1], 1, MPI_INT, dir_baixo_tras_rank, 0, &vert_esq_cima_frente, 1, MPI_INT, esq_cima_frente_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR dir cima
-    MPI_Sendrecv(&data_send[TAMANHO_GRID-1][TAMANHO_GRID-1][1], 1, MPI_INT, esq_cima_frente_rank, 0, &vert_dir_baixo_tras, 1, MPI_INT, dir_baixo_tras_rank, 0, cart_comm, MPI_STATUS_IGNORE);
-    MPI_Sendrecv(&data_send[TAMANHO_GRID-1][TAMANHO_GRID-1][TAMANHO_GRID-1], 1, MPI_INT, dir_cima_frente_rank, 0, &vert_esq_baixo_tras, 1, MPI_INT, esq_baixo_tras_rank, 0, cart_comm, MPI_STATUS_IGNORE);
+    MPI_Sendrecv(&data_send[1][1][TAMANHO_GRID], 1, MPI_INT, dir_baixo_tras_rank, 0, &vert_esq_cima_frente, 1, MPI_INT, esq_cima_frente_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR dir cima
+    MPI_Sendrecv(&data_send[TAMANHO_GRID][TAMANHO_GRID][1], 1, MPI_INT, esq_cima_frente_rank, 0, &vert_dir_baixo_tras, 1, MPI_INT, dir_baixo_tras_rank, 0, cart_comm, MPI_STATUS_IGNORE);
+    MPI_Sendrecv(&data_send[TAMANHO_GRID][TAMANHO_GRID][TAMANHO_GRID], 1, MPI_INT, dir_cima_frente_rank, 0, &vert_esq_baixo_tras, 1, MPI_INT, esq_baixo_tras_rank, 0, cart_comm, MPI_STATUS_IGNORE);
     MPI_Sendrecv(&data_send[1][1][1], 1, MPI_INT, esq_baixo_tras_rank, 0, &vert_dir_cima_frente, 1, MPI_INT, dir_cima_frente_rank, 0, cart_comm, MPI_STATUS_IGNORE);
     
-    MPI_Sendrecv(&data_send[1][TAMANHO_GRID-1][TAMANHO_GRID-1], 1, MPI_INT, dir_baixo_frente_rank, 0, &vert_esq_cima_tras, 1, MPI_INT, esq_cima_tras_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR dir cima
-    MPI_Sendrecv(&data_send[TAMANHO_GRID-1][1][1], 1, MPI_INT, esq_cima_tras_rank, 0, &vert_dir_baixo_frente, 1, MPI_INT, dir_baixo_frente_rank, 0, cart_comm, MPI_STATUS_IGNORE);
-    MPI_Sendrecv(&data_send[TAMANHO_GRID-1][1][TAMANHO_GRID-1], 1, MPI_INT, dir_cima_tras_rank, 0, &vert_esq_baixo_frente, 1, MPI_INT, esq_baixo_frente_rank, 0, cart_comm, MPI_STATUS_IGNORE);
-    MPI_Sendrecv(&data_send[1][TAMANHO_GRID-1][1], 1, MPI_INT, esq_baixo_frente_rank, 0, &vert_dir_cima_tras, 1, MPI_INT, dir_cima_tras_rank, 0, cart_comm, MPI_STATUS_IGNORE);
+    MPI_Sendrecv(&data_send[1][TAMANHO_GRID][TAMANHO_GRID], 1, MPI_INT, dir_baixo_frente_rank, 0, &vert_esq_cima_tras, 1, MPI_INT, esq_cima_tras_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR dir cima
+    MPI_Sendrecv(&data_send[TAMANHO_GRID][1][1], 1, MPI_INT, esq_cima_tras_rank, 0, &vert_dir_baixo_frente, 1, MPI_INT, dir_baixo_frente_rank, 0, cart_comm, MPI_STATUS_IGNORE);
+    MPI_Sendrecv(&data_send[TAMANHO_GRID][1][TAMANHO_GRID], 1, MPI_INT, dir_cima_tras_rank, 0, &vert_esq_baixo_frente, 1, MPI_INT, esq_baixo_frente_rank, 0, cart_comm, MPI_STATUS_IGNORE);
+    MPI_Sendrecv(&data_send[1][TAMANHO_GRID][1], 1, MPI_INT, esq_baixo_frente_rank, 0, &vert_dir_cima_tras, 1, MPI_INT, dir_cima_tras_rank, 0, cart_comm, MPI_STATUS_IGNORE);
 
     for( aux_z=0; aux_z < TAMANHO_GRID; aux_z++)
     {
