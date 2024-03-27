d838f986d8ab4c8c32a9d9804de22a819d7ad1c4
olivas-afonso <olivas.afonso@gmail.com>
2024-03-27 19:57:42 +0000

    Update quase_la.c

--- mpi testes to run/quase_la.c
+++ mpi testes to run/quase_la.c
@@ -340,11 +340,11 @@ int main(int argc, char *argv[]) {
 
     
     MPI_Sendrecv(&data_send[1][1][sub_x], 1, MPI_INT, esq_cima_frente_rank, 0, &data_send[sub_z+1][sub_y+1][0], 1, MPI_INT, dir_baixo_tras_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR dir cima
-    MPI_Sendrecv(&data_send[sub_z][sub_y][1], 1, MPI_INT, edir_baixo_tras_rank, 0, &data_send[0][0][sub_x+1], 1, MPI_INT, esq_cima_frente_rank, 0, cart_comm, MPI_STATUS_IGNORE);
+    MPI_Sendrecv(&data_send[sub_z][sub_y][1], 1, MPI_INT, dir_baixo_tras_rank, 0, &data_send[0][0][sub_x+1], 1, MPI_INT, esq_cima_frente_rank, 0, cart_comm, MPI_STATUS_IGNORE);
     MPI_Sendrecv(&data_send[sub_z][sub_y][sub_x], 1, MPI_INT, esq_baixo_tras_rank, 0, &data_send[0][0][0], 1, MPI_INT, dir_cima_frente_rank, 0, cart_comm, MPI_STATUS_IGNORE);
     MPI_Sendrecv(&data_send[1][1][1], 1, MPI_INT, dir_cima_frente_rank, 0, &data_send[sub_z+1][sub_y+1][sub_x+1], 1, MPI_INT, dir_cima_frente_rank, 0, cart_comm, MPI_STATUS_IGNORE);
     
-    MPI_Sendrecv(&data_send[1][sub_y][sub_x], 1, MPI_INT, desq_cima_tras_rank, 0, &data_send[sub_z+1][0][0], 1, MPI_INT, dir_baixo_frente_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR dir cima
+    MPI_Sendrecv(&data_send[1][sub_y][sub_x], 1, MPI_INT, esq_cima_tras_rank, 0, &data_send[sub_z+1][0][0], 1, MPI_INT, dir_baixo_frente_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR dir cima
     MPI_Sendrecv(&data_send[sub_z][1][1], 1, MPI_INT, dir_baixo_frente_rank, 0, &data_send[0][sub_y+1][sub_x+1], 1, MPI_INT, esq_cima_tras_rank, 0, cart_comm, MPI_STATUS_IGNORE);
     MPI_Sendrecv(&data_send[sub_z][1][sub_x], 1, MPI_INT, esq_baixo_frente_rank, 0, &data_send[0][sub_y+1][0], 1, MPI_INT, dir_cima_tras_rank, 0, cart_comm, MPI_STATUS_IGNORE);
     MPI_Sendrecv(&data_send[1][sub_y][1], 1, MPI_INT, dir_cima_tras_rank, 0, &data_send[sub_z+1][0][sub_x+1], 1, MPI_INT, esq_baixo_frente_rank, 0, cart_comm, MPI_STATUS_IGNORE);
@@ -359,7 +359,7 @@ int main(int argc, char *argv[]) {
 
         //FACE ESQUERDA DIAGS
         MPI_Sendrecv(&data_send[aux_z+1][1][sub_x], 1, MPI_INT, esq_frente_rank, 0, &data_send[aux_z+1][sub_y+1][0], 1, MPI_INT, dir_tras_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR esq baixo
-        MPI_Sendrecv(&data_send[aux_z+1][sub_y][sub_x], 1, MPI_INT, esq_tras_rank, 0, &data_send[aux_z+1][0][0], 1, MPI_INT, dir_esquerda_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR esq baixo
+        MPI_Sendrecv(&data_send[aux_z+1][sub_y][sub_x], 1, MPI_INT, esq_tras_rank, 0, &data_send[aux_z+1][0][0], 1, MPI_INT, dir_frente_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR esq baixo
         
         
         for (aux_y=0; aux_y<sub_y; aux_y++)
