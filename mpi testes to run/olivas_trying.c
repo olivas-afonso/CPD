99fa9e8a665743fc3da76a0467ca5aa2b7a7b948
olivas-afonso <olivas.afonso@gmail.com>
2024-03-27 23:08:48 +0000

    Update olivas_trying.c

--- mpi testes to run/olivas_trying.c
+++ mpi testes to run/olivas_trying.c
@@ -415,7 +415,7 @@ int main(int argc, char *argv[]) {
     }
 
     //--------------------------------------DEBUG-----------------------------------------------
-    if(rank==5)
+    if(rank==0)
     {
         //printf("RANK: %d    SUB_Z: %d   SUB_Y: %d   SUB_X:  %d\n",rank, sub_z, sub_y, sub_x);
             //MPI_Cart_coords(cart_comm, rank, 3, my_coords);
