3f41a8871abd39a14748323c63b8bad8cfc15084
olivas-afonso <olivas.afonso@gmail.com>
2024-03-27 18:02:33 +0000

    Update AHHHH2.c

--- mpi testes to run/AHHHH2.c
+++ mpi testes to run/AHHHH2.c
@@ -289,18 +289,11 @@ int main(int argc, char *argv[]) {
 
         for (int init_z=0; init_z < NUM_LINHAS; init_z++){
             
-             if(r4_uni() < density)
-            {
-                // preenchimento initial do grid_even dependendo da seed
-                valor_aux = (int)(r4_uni() * N_SPECIES) + 1; // preenchimento initial do grid_even dependendo da seed
 
-            }else{
-                valor_aux = 0;
-            }
 
-            if (init_z>=limite_inf_y && init_z<limite_sup_y && flag_x == 1 && flag_y == 1){
+            if (init_z>=limite_inf_y && init_z<limite_sup_y && flag_x == 1 && flag_y == 1 && r4_uni() < density){
                   
-                  data_send[varrimento_x-1][varrimento_y-1][varrimento_z] = valor_aux;
+                  data_send[varrimento_x-1][varrimento_y-1][varrimento_z] = (int)(r4_uni() * N_SPECIES) + 1;
                   printf("VALORES A ENTRAR %d, pos_x = %d, pos_y = %d, pos_z = %d \n", valor_aux, varrimento_x-1, varrimento_y-1, varrimento_z);
                  ++varrimento_z;
             }
