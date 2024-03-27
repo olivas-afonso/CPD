60100cbaef8f3cb0b915a4550a00b0d79308a2c4
olivas-afonso <olivas.afonso@gmail.com>
2024-03-27 18:13:32 +0000

    Update AHHHH2.c

--- mpi testes to run/AHHHH2.c
+++ mpi testes to run/AHHHH2.c
@@ -297,7 +297,7 @@ int main(int argc, char *argv[]) {
                         valor_aux = 0;
                     }
 
-            if (init_z>=limite_inf_y && init_z<limite_sup_y && flag_x == 1 && flag_y == 1 ){
+            if (init_z>=limite_inf_z && init_z<limite_sup_z && flag_x == 1 && flag_y == 1 ){
 
                 data_send[varrimento_x-1][varrimento_y-1][varrimento_z] = valor_aux;
                   printf("VALORES A ENTRAR %d, pos_x = %d, pos_y = %d, pos_z = %d \n", data_send[varrimento_x-1][varrimento_y-1][varrimento_z], varrimento_x-1, varrimento_y-1, varrimento_z);
