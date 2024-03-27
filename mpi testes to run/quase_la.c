223f8abf2bc292b2f6c70d95dae365254e8a0a2e
olivas-afonso <olivas.afonso@gmail.com>
2024-03-27 18:51:10 +0000

    Update quase_la.c

--- mpi testes to run/quase_la.c
+++ mpi testes to run/quase_la.c
@@ -413,7 +413,7 @@ int main(int argc, char *argv[]) {
     }
 
     //--------------------------------------DEBUG-----------------------------------------------
-    if(rank==2)
+    if(rank==0)
     {
         //printf("RANK: %d    SUB_Z: %d   SUB_Y: %d   SUB_X:  %d\n",rank, sub_z, sub_y, sub_x);
             //MPI_Cart_coords(cart_comm, rank, 3, my_coords);
