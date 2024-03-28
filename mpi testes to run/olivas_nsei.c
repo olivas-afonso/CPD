4d9fed44baad4569716e2ecd5de26cb30a6948e2
Duarte Diamantino <62574192+Duarte-Diamantino@users.noreply.github.com>
2024-03-28 22:59:19 +0000

    Update olivas_nsei.c

--- mpi testes to run/olivas_nsei.c
+++ mpi testes to run/olivas_nsei.c
@@ -197,6 +197,7 @@ void verifica_max (int *max_gen, int gen_number){
             if(count_species[x] > max_count[x])
             {   
                 max_count[x] = count_species[x];
+                printf("maximos iniciais: %d\n", max_count);
                 max_gen[x]=gen_number;
             }
         }    
