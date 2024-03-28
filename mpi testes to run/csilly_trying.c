cdaadd0d25bf9d626020a95c3c158d22fac137c6
GoncaloCecilio12 <145784765+GoncaloCecilio12@users.noreply.github.com>
2024-03-28 19:52:03 +0000

    Update csilly_trying.c

--- mpi testes to run/csilly_trying.c
+++ mpi testes to run/csilly_trying.c
@@ -441,11 +441,13 @@ void rules(int sub_x ,int sub_y, int sub_z , char ***grid_new, char ***grid_old)
     //{
         //#pragma omp for schedule (dynamic)
         
-        for(aux_z=1; aux_z<= sub_z; aux_z ++)
-        {
-            for(aux_y=1; aux_y<= sub_y; aux_y++)
+        for(aux_z=1; aux_z< sub_z; aux_z ++)
+        {   
+            if (rank == 0)
+                printf ("ENTREI EM Z\n");
+            for(aux_y=1; aux_y< sub_y; aux_y++)
             {
-                for(aux_x=1; aux_x<= sub_x; aux_x++)
+                for(aux_x=1; aux_x< sub_x; aux_x++)
                 {
                     if(grid_old[aux_z][aux_y][aux_x]==0) // morto 
                     { 
