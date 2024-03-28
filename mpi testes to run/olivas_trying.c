ec8992b124e850aa35c7a62ed4121bf0a660be2e
olivas-afonso <olivas.afonso@gmail.com>
2024-03-28 00:42:18 +0000

    Update olivas_trying.c

--- mpi testes to run/olivas_trying.c
+++ mpi testes to run/olivas_trying.c
@@ -409,7 +409,7 @@ MPI_Barrier(cart_comm);
         {
             
             
-            printf("VEZES\n");
+            if(rank==0) printf("VEZES\n");
             //FACE DIREITA 
             MPI_Sendrecv(&data_send[aux_z+1][aux_y+1][1], 1, MPI_INT, esq_rank, 0, &data_send[aux_z+1][aux_y+1][sub_x+1], 1, MPI_INT, dir_rank, 0, cart_comm, MPI_STATUS_IGNORE); // face dir
 
