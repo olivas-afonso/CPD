f3cda21a672a5f8ef00bca68eaa4a9759da9ed54
olivas-afonso <olivas.afonso@gmail.com>
2024-03-28 00:38:15 +0000

    Update olivas_trying.c

--- mpi testes to run/olivas_trying.c
+++ mpi testes to run/olivas_trying.c
@@ -408,6 +408,7 @@ MPI_Barrier(cart_comm);
         for (aux_y=0; aux_y<sub_y; aux_y++)
         {
 
+            MPI_Sendrecv(&data_send[aux_z+1][aux_y+1][sub_x], 1, MPI_INT, dir_rank, 0, &data_send[aux_z+1][aux_y+1][0], 1, MPI_INT, esq_rank, 0, cart_comm, MPI_STATUS_IGNORE); // face dir
             
             //FACE DIREITA 
             MPI_Sendrecv(&data_send[aux_z+1][aux_y+1][1], 1, MPI_INT, esq_rank, 0, &data_send[aux_z+1][aux_y+1][sub_x+1], 1, MPI_INT, dir_rank, 0, cart_comm, &status); // face dir
@@ -424,7 +425,7 @@ MPI_Barrier(cart_comm);
             printf("DATA HELLO PLEASE FOR FUCK SAKE: %d \n",  data_send[aux_z+1][aux_y+1][sub_x+1]);
             */
             //FACE ESQUERDA 
-            MPI_Sendrecv(&data_send[aux_z+1][aux_y+1][sub_x], 1, MPI_INT, dir_rank, 0, &data_send[aux_z+1][aux_y+1][0], 1, MPI_INT, esq_rank, 0, cart_comm, MPI_STATUS_IGNORE); // face dir
+            //MPI_Sendrecv(&data_send[aux_z+1][aux_y+1][sub_x], 1, MPI_INT, dir_rank, 0, &data_send[aux_z+1][aux_y+1][0], 1, MPI_INT, esq_rank, 0, cart_comm, MPI_STATUS_IGNORE); // face dir
 
         }
         /*
