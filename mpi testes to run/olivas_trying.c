ac1ca1a8d00af1b1cd80cb17334862a62e4b8cf0
olivas-afonso <olivas.afonso@gmail.com>
2024-03-28 00:14:09 +0000

    Update olivas_trying.c

--- mpi testes to run/olivas_trying.c
+++ mpi testes to run/olivas_trying.c
@@ -378,10 +378,7 @@ MPI_Barrier(cart_comm);
 
     
 
-    MPI_Cart_coords(cart_comm, rank, 3, my_coords);
-    sub_z = sub_divz_z[my_coords[0]];
-    sub_y = sub_divz_y[my_coords[1]];
-    sub_x = sub_divz_x[my_coords[2]];
+    
 
     
     MPI_Sendrecv(&data_send[1][1][sub_x], 1, MPI_INT, dir_baixo_tras_rank, 0, &data_send[sub_z+1][sub_y+1][0], 1, MPI_INT, esq_cima_frente_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR dir cima
@@ -396,10 +393,7 @@ MPI_Barrier(cart_comm);
 
     for( aux_z=0; aux_z < sub_z; aux_z++)
     {
-        MPI_Cart_coords(cart_comm, rank, 3, my_coords);
-        sub_z = sub_divz_z[my_coords[0]];
-        sub_y = sub_divz_y[my_coords[1]];
-        sub_x = sub_divz_x[my_coords[2]];
+        printf("%d %d %d", aux_z, aux_y, aux_z);
         //FACE DIREITA DIAGS
         MPI_Sendrecv(&data_send[aux_z+1][1][1], 1, MPI_INT, esq_tras_rank, 0, &data_send[aux_z+1][sub_y+1][sub_x+1], 1, MPI_INT, dir_frente_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR dir cima
         MPI_Sendrecv(&data_send[aux_z+1][sub_y][1], 1, MPI_INT, esq_frente_rank, 0, &data_send[aux_z+1][0][sub_x+1], 1, MPI_INT, dir_tras_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR dir baixo
@@ -411,10 +405,7 @@ MPI_Barrier(cart_comm);
         
         for (aux_y=0; aux_y<sub_y; aux_y++)
         {
-            MPI_Cart_coords(cart_comm, rank, 3, my_coords);
-            sub_z = sub_divz_z[my_coords[0]];
-            sub_y = sub_divz_y[my_coords[1]];
-            sub_x = sub_divz_x[my_coords[2]];
+            printf("SAO ESTES: %d %d %d", aux_z, aux_y, aux_z);
             //FACE DIREITA 
             MPI_Sendrecv(&data_send[aux_z+1][aux_y+1][1], 1, MPI_INT, esq_rank, 0, &data_send[aux_z+1][aux_y+1][sub_x+1], 1, MPI_INT, dir_rank, 0, cart_comm, &status); // face dir
             
@@ -424,10 +415,7 @@ MPI_Barrier(cart_comm);
         }
         for(aux_x=0; aux_x<sub_x; aux_x++)
         {
-            MPI_Cart_coords(cart_comm, rank, 3, my_coords);
-            sub_z = sub_divz_z[my_coords[0]];
-            sub_y = sub_divz_y[my_coords[1]];
-            sub_x = sub_divz_x[my_coords[2]];
+            printf("%d %d %d", aux_z, aux_y, aux_z);
             //FACE FRENTE
             MPI_Sendrecv(&data_send[aux_z+1][1][aux_x+1], 1, MPI_INT, tras_rank, 0, &data_send[aux_z+1][sub_y+1][aux_x+1], 1, MPI_INT, frente_rank, 0, cart_comm, MPI_STATUS_IGNORE); // face dir   
             
@@ -439,10 +427,7 @@ MPI_Barrier(cart_comm);
     
     for (aux_y=0; aux_y<sub_y; aux_y++)
     {
-        MPI_Cart_coords(cart_comm, rank, 3, my_coords);
-        sub_z = sub_divz_z[my_coords[0]];
-        sub_y = sub_divz_y[my_coords[1]];
-        sub_x = sub_divz_x[my_coords[2]];
+        printf("%d %d %d", aux_z, aux_y, aux_z);
         //FACE CIMA DIAGS
         MPI_Sendrecv(&data_send[1][aux_y+1][sub_x],1, MPI_INT, dir_baixo_rank, 0, &data_send[sub_z+1][aux_y+1][0], 1, MPI_INT, esq_cima_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR esq baixo
         MPI_Sendrecv(&data_send[1][aux_y+1][1], 1, MPI_INT, esq_baixo_rank, 0, &data_send[sub_z+1][aux_y+1][sub_x+1], 1, MPI_INT, dir_cima_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR dir cima
@@ -453,10 +438,7 @@ MPI_Barrier(cart_comm);
 
         for(aux_x=0; aux_x<sub_x; aux_x++)
         {
-            MPI_Cart_coords(cart_comm, rank, 3, my_coords);
-            sub_z = sub_divz_z[my_coords[0]];
-            sub_y = sub_divz_y[my_coords[1]];
-            sub_x = sub_divz_x[my_coords[2]];
+            printf("%d %d %d", aux_z, aux_y, aux_z);
             //FACE CIMA
             MPI_Sendrecv(&data_send[1][aux_y+1][aux_x+1], 1, MPI_INT, baixo_rank, 0, &data_send[sub_z+1][aux_y+1][aux_x+1], 1, MPI_INT, cima_rank, 0, cart_comm, MPI_STATUS_IGNORE); // face dir  
             
@@ -467,10 +449,7 @@ MPI_Barrier(cart_comm);
 
     for(aux_x=0; aux_x<sub_x; aux_x++)
     {
-        MPI_Cart_coords(cart_comm, rank, 3, my_coords);
-        sub_z = sub_divz_z[my_coords[0]];
-        sub_y = sub_divz_y[my_coords[1]];
-        sub_x = sub_divz_x[my_coords[2]];
+        printf("%d %d %d", aux_z, aux_y, aux_z);
         //FACE FRENTE DIAGS
         MPI_Sendrecv(&data_send[sub_z][1][aux_x+1], 1, MPI_INT, tras_cima_rank, 0, &data_send[0][sub_y+1][aux_x+1], 1, MPI_INT, frente_baixo_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR esq baixo
         MPI_Sendrecv(&data_send[1][1][aux_x+1], 1, MPI_INT, tras_baixo_rank, 0, &data_send[sub_z+1][sub_y+1][aux_x+1], 1, MPI_INT, frente_cima_rank, 0, cart_comm, MPI_STATUS_IGNORE); // AR dir cima
