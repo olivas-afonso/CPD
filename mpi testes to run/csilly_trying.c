ec1c96769bbdead1aa394844cb77c84a3310c805
GoncaloCecilio12 <145784765+GoncaloCecilio12@users.noreply.github.com>
2024-03-28 21:58:57 +0000

    Update csilly_trying.c

--- mpi testes to run/csilly_trying.c
+++ mpi testes to run/csilly_trying.c
@@ -185,8 +185,7 @@ void aloca_matrizes (int sub_x, int sub_y, int sub_z){
 
 void verifica_max (int *max_gen, int gen_number){
     MPI_Barrier(MPI_COMM_WORLD);
-    memset (count_species, 0 ,sizeof(count_species));
-    MPI_Allreduce(count_species_local, count_species, sizeof (count_species), MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+    MPI_Reduce(count_species_local, count_species, sizeof (count_species), MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
     MPI_Barrier(MPI_COMM_WORLD);
     /*for (int x=1; x< 10; ++x){
         printf ("X=%d Cnt=%d\n", x, count_species[x]);
@@ -382,7 +381,7 @@ int death_rule(char *** grid, long aux_x, long aux_y, long aux_z)
         max=cont_species_death[0];
         
         max_pos=0;
-        for(i=0; i <9;i++ )
+        for(i=0; i <9;i++)
         {
             if(cont_species_death[i]>max)
             {
