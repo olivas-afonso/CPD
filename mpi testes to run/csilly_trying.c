535e7bd58cf7c6b61cd0e0671bdada2c7b47ffa1
GoncaloCecilio12 <145784765+GoncaloCecilio12@users.noreply.github.com>
2024-03-28 21:07:17 +0000

    Update csilly_trying.c

--- mpi testes to run/csilly_trying.c
+++ mpi testes to run/csilly_trying.c
@@ -600,7 +600,7 @@ int main(int argc, char *argv[]) {
     if (rank == 0){
         for(int auxi=1; auxi < 10; auxi++)
         {
-            printf("%d %ld %d \n", auxi, max_count[auxi], max_gen[auxi]);
+            printf("%d \n",  max_gen[auxi]);
         }
     }
 
