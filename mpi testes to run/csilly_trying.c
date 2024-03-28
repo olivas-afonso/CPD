b33dac5df4234c2cc23f3d8ee7a26e2939cc29af
GoncaloCecilio12 <145784765+GoncaloCecilio12@users.noreply.github.com>
2024-03-28 20:12:56 +0000

    Update csilly_trying.c

--- mpi testes to run/csilly_trying.c
+++ mpi testes to run/csilly_trying.c
@@ -181,7 +181,7 @@ void aloca_matrizes (int sub_x, int sub_y, int sub_z){
 void verifica_max (int gen_number){
     
     MPI_Reduce(count_species_local, count_species, sizeof (count_species), MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-    MPI_Barrier(MPI_COMM_WORLD);
+    //MPI_Barrier(MPI_COMM_WORLD);
     /*for (int x=1; x< 10; ++x){
         printf ("X=%d Cnt=%d\n", x, count_species[x]);
     }*/
