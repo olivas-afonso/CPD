011626fc96811668afea5987abfc1af072b82bde
GoncaloCecilio12 <145784765+GoncaloCecilio12@users.noreply.github.com>
2024-03-29 00:41:36 +0000

    Update vou_tentar_meter_mp.c

--- mpi testes to run/vou_tentar_meter_mp.c
+++ mpi testes to run/vou_tentar_meter_mp.c
@@ -475,8 +475,8 @@ void freeMatrix(int sub_y, int sub_z) {
     free(grid_even);
     free(grid_odd);
     free (max_gen);
-   /* free (count_species);
-    free (sub_divz_x);
+    free (count_species);
+  /*free (sub_divz_x);
     free (sub_divz_y);
     free (sub_divz_x);
     free (count_species_new);    */
