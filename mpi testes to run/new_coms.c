061c853c9af200e61c6f23c62030bb7e7ef6a856
olivas-afonso <olivas.afonso@gmail.com>
2024-03-29 19:45:18 +0000

    Update new_coms.c

--- mpi testes to run/new_coms.c
+++ mpi testes to run/new_coms.c
@@ -754,7 +754,7 @@ int main(int argc, char *argv[]) {
         }
                
         MPI_Barrier(MPI_COMM_WORLD);
-        MPI_Reduce(count_species_local, count_species, 10, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+        MPI_Reduce(count_species_local, count_species, 10, MPI_LONG, MPI_SUM, 0, cart_comm);
         MPI_Barrier(MPI_COMM_WORLD);
 
         if(rank==0)
